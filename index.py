0805cc72e543011d95dceef0872bed1e283fa303
ajinkyalab2 <prachigirme021@gmail.com>
2025-10-08 15:36:43 +0530

    this is edited by user 2

--- index.py
+++ index.py
@@ -1,2 +1,6 @@
 this is a demo file to check the merge conflicts issue
-"this is edited by user 1" 
+<<<<<<< HEAD
+ 
+=======
+"this is edited by user 2" 
+>>>>>>> 7b41e25 (this is edited by user 2)
